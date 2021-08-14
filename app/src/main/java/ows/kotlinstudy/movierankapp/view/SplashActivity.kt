48bf4fd43a5b0821fbec018554cba154e0986844
ows3090 <ows3090@naver.com>
2021-08-15 02:39:59 +0900

    init commit

---
+++ app/src/main/java/ows/kotlinstudy/movierankapp/view/SplashActivity.kt
@@ -0,0 +1,15 @@
+package ows.kotlinstudy.movierankapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ows.kotlinstudy.movierankapp.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+
+    }
+}
