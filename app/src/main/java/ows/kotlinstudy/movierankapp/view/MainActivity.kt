d5fffbb41ddad3d4671e4c4ddfab0a336cdb1eec
ows3090 <ows3090@naver.com>
2021-08-16 03:00:04 +0900

    feat : MainActivity DrawerLayout and Custom Toolbar

--- app/src/main/java/ows/kotlinstudy/movierankapp/view/MainActivity.kt
+++ app/src/main/java/ows/kotlinstudy/movierankapp/view/MainActivity.kt
@@ -1,12 +1,17 @@
 package ows.kotlinstudy.movierankapp.view
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import ows.kotlinstudy.movierankapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
     }
 }
